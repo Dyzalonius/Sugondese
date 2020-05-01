c0a66c35679870298dfce71454b727d0b3cb78c7
dyzalonius <dyzalonius@gmail.com>
2020-05-01 12:37:35 +0200

    Add Photon and main menu and lobby flow

---
+++ Assets/Scripts/ClientItem.cs
@@ -0,0 +1,54 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClientItem : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    private TextMeshProUGUI clientNameText;
+
+    [SerializeField]
+    private TextMeshProUGUI readyText;
+
+    [SerializeField]
+    private TextMeshProUGUI readyButtonText;
+
+    [SerializeField]
+    private Button readyButton;
+
+    private bool isReady;
+
+    public void Setup(Photon.Realtime.Player player)
+    {
+        if (player.CustomProperties.ContainsKey("isReady"))
+            isReady = (bool)player.CustomProperties["isReady"];
+        else
+            isReady = false; //TODO: Check if this is necessary
+        clientNameText.text = "player#" + player.ActorNumber;
+        readyButtonText.text = isReady ? "Unready" : "Ready";
+        readyText.text = isReady ? "Ready" : "Waiting";
+
+
+        if (player != PhotonNetwork.LocalPlayer)
+            readyButton.gameObject.SetActive(false);
+        else
+            readyButton.onClick.AddListener(ToggleReady);
+    }
+
+    public void ToggleReady()
+    {
+        isReady = !isReady;
+        LobbyManager.Instance.ClickReadyButton(PhotonNetwork.LocalPlayer, isReady);
+    }
+
+    // Only called by RPC from LobbyManager
+    public void DisplayIsReady(bool isReady)
+    {
+        readyButtonText.text = isReady ? "Unready" : "Ready";
+        readyText.text = isReady ? "Ready" : "Waiting";
+    }
+}
---
+++ Assets/Scripts/ClientList.cs
@@ -0,0 +1,50 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientList : MonoBehaviour
+{
+    [SerializeField]
+    private ClientItem clientItemPrefab;
+
+    private List<Photon.Realtime.Player> players = new List<Photon.Realtime.Player>();
+    private Dictionary<Photon.Realtime.Player, ClientItem> clientItems = new Dictionary<Photon.Realtime.Player, ClientItem>();
+
+    public void UpdateList()
+    {
+        // Remove old ones
+        for (int i = players.Count - 1; i >= 0; i--)
+        {
+            if (!PhotonNetwork.CurrentRoom.Players.ContainsValue(players[i]))
+                RemoveClientItem(players[i]);
+        }
+
+        // Add new ones
+        foreach (var pair in PhotonNetwork.CurrentRoom.Players)
+        {
+            if (!players.Contains(pair.Value))
+                AddClientItem(pair.Value);
+        }
+    }
+
+    private void AddClientItem(Photon.Realtime.Player player)
+    {
+        ClientItem newClientItem = Instantiate(clientItemPrefab, transform).GetComponent<ClientItem>();
+        newClientItem.Setup(player);
+        players.Add(player);
+        clientItems.Add(player, newClientItem);
+    }
+
+    private void RemoveClientItem(Photon.Realtime.Player player)
+    {
+        ClientItem clientItem = clientItems[player];
+        clientItems.Remove(player);
+        Destroy(clientItem);
+    }
+
+    public ClientItem GetClientItem(Photon.Realtime.Player player)
+    {
+        return clientItems[player];
+    }
+}
