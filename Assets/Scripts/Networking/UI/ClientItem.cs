5435318cc313b38c2f411db09cbc699281406b40
dyzalonius <dyzalonius@gmail.com>
2020-10-06 12:09:45 +0200

    Add distinction in player list for local player

--- Assets/Scripts/Networking/UI/ClientItem.cs
+++ Assets/Scripts/Networking/UI/ClientItem.cs
@@ -15,9 +15,11 @@ public class ClientItem : MonoBehaviour
     public void Setup(Player player)
     {
         clientNameText.text = player.NickName;
+        clientNameText.fontStyle = player.IsLocal ? FontStyles.Bold : FontStyles.Normal;
 
         bool isMasterClient = player.ActorNumber == PhotonNetwork.CurrentRoom.MasterClientId; // Alternative to player.isMasterClient, because that variable isn't always correct
         bool isReady = player.CustomProperties.ContainsKey("isReady") ? (bool)player.CustomProperties["isReady"] : false;
         readyText.text = isMasterClient ? "Host" : isReady ? "Ready" : "Not Ready";
+        readyText.fontStyle = player.IsLocal ? FontStyles.Bold : FontStyles.Normal;
     }
 }
